77c81c1909bc382441765cbbf603c908aa98cc89
Hakeem <hakeem@Hakeems-MacBook-Pro.local>
2025-07-02 04:20:57 +0100

    Fixed puppeteer auth bugs

--- src/index.js
+++ src/index.js
@@ -1,28 +1,42 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import { browser, page } from './utils/browser';
-import scrape from './utils/scrape';
+import { browser, page } from './utils/browser.js';
+import scrape from './utils/scrape.js';
 
 await page.goto(`https://www.instagram.com/salsamanthaa`);
 
 try {
     let numOfFollowers = await page.evaluate(async () => {
         const elem = document.querySelector('#mount_0_0_J8 > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6.x1ja2u2z.x10cihs4 > div:nth-child(2) > div > div.xvc5jky.xh8yej3.x10o80wk.x14k21rp.x17snn68.x6osk4m.x1porb0y.x8vgawa > section > main > div > header > section.xc3tme8.x1xdureb.x18wylqe.x13vxnyz.xvxrpd7 > ul > li:nth-child(2) > div > a > span > span > span');
-        return elem.textContent
+        return elem ? elem.textContent : null;
     })
 
     if(numOfFollowers !== null) {
         // Main logic. Should be abstracted away into a function
-        scrape()
+        console.log('Puppeteer is authenticated');
+        scrape(Number(numOfFollowers));
+        process.exit(0)
     }
 
     // Authenticate puppeteer
-    await page.locator('#mount_0_0_il > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6.x1ja2u2z.x10cihs4 > div:nth-child(2) > div > div.x10o80wk.x14k21rp.xh8yej3.x8vgawa > section > div > div > div._acuq._acur > div > div > div > div:nth-child(1) > a').click();
-    await page.type('//*[@id="loginForm"]/div[1]/div[1]/div/label/input', process.env.USERNAME, { delay: 500 });
-    await page.type('//*[@id="loginForm"]/div[1]/div[2]/div/label/input', process.env.PASSWORD, { delay: 500 });
-    await page.locator('//*[@id="loginForm"]/div[1]/div[3]/button/div').click();
-
-    scrape();
+    console.log('Puppeteer is trying to authenticate');
+    await page.goto('https://instagram.com');
+    console.log('Navigated to login page')
+    await page.type('#loginForm > div.x9f619.xjbqb8w.x78zum5.x15mokao.x1ga7v0g.x16uus16.xbiv7yw.xqui205.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1 > div:nth-child(1) > div > label > input', process.env.USERNAME, { delay: 500 });
+    console.log('Filled username field');
+    await page.type('#loginForm > div.x9f619.xjbqb8w.x78zum5.x15mokao.x1ga7v0g.x16uus16.xbiv7yw.xqui205.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1 > div:nth-child(2) > div > label > input', process.env.PASSWORD, { delay: 500 });
+    console.log('Filled password field');
+    await page.evaluate(() => {
+        document.querySelector('#loginForm > div.x9f619.xjbqb8w.x78zum5.x15mokao.x1ga7v0g.x16uus16.xbiv7yw.xqui205.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1 > div:nth-child(3) > button').click()
+    })
+    console.log('Clicked login button');
+    await page.waitForNavigation();
+    await page.goto('https://www.instagram.com/salsamanthaa');
+    console.log('User is now authenticted');
+    const elem = await page.evaluate(() => {
+        return document.querySelector('#mount_0_0_J8 > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6.x1ja2u2z.x10cihs4 > div:nth-child(2) > div > div.xvc5jky.xh8yej3.x10o80wk.x14k21rp.x17snn68.x6osk4m.x1porb0y.x8vgawa > section > main > div > header > section.xc3tme8.x1xdureb.x18wylqe.x13vxnyz.xvxrpd7 > ul > li:nth-child(2) > div > a > span > span > span');
+    })
+    scrape(Number(elem.textContent));
 
 } catch(error) {
     console.log(error);
--- src/utils/scrape.js
+++ src/utils/scrape.js
@@ -1,13 +1,13 @@
-import { page } from './browser';
+import { page } from './browser.js';
 const usernameMap = new Map();
 
 export default async function scrape(numOfFollowers) {
-    // Instagram could prompt puppeteer to save login info
-
     try {
-
+        console.log('Ready for scraping');
+        console.log(numOfFollowers);
         for (let i = 0; i < numOfFollowers; i++) {
             await page.locator('//*[@id="mount_0_0_0u"]/div/div/div[2]/div/div/div[1]/div[2]/div/div[1]/section/main/div/header/section[3]/ul/li[2]/div/a/span/span/span').click();
+            console.log('Clicked followers btn');
             const username = await page.evaluate(() => {
                 const followersContainer = document.querySelector('body > div.x1n2onr6.xzkaem6 > div:nth-child(2) > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.x1bphaa0.x18nydb4.xcm95gh.x1vsb9q8.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div.x6nl9eh.x1a5l9x9.x7vuprf.x1mg3h75.x1lliihq.x1iyjqo2.xs83m0k.xz65tgg.x1rife3k.x1n2onr6 > div:nth-child(1) > div');
                 return followersContainer.children[i].querySelector('body > div.x1n2onr6.xzkaem6 > div:nth-child(2) > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.x1bphaa0.x18nydb4.xcm95gh.x1vsb9q8.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div.x6nl9eh.x1a5l9x9.x7vuprf.x1mg3h75.x1lliihq.x1iyjqo2.xs83m0k.xz65tgg.x1rife3k.x1n2onr6 > div:nth-child(1) > div > div:nth-child(1) > div > div > div > div.x9f619.x1ja2u2z.x78zum5.x1n2onr6.x1iyjqo2.xs83m0k.xeuugli.x1qughib.x6s0dn4.x1a02dak.x1q0g3np.xdl72j9 > div > div > div > div > span > div > a > div > div > span').textContent;
